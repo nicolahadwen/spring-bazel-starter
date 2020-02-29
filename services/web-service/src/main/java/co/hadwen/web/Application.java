387dff0ee33344017da898609277bbf0045138b6
nicola <nicola@ritual.co>
2020-02-29 13:20:31 -0500

    import config to app

--- services/web-service/src/main/java/co/hadwen/web/Application.java
+++ services/web-service/src/main/java/co/hadwen/web/Application.java
@@ -3,7 +3,9 @@ package co.hadwen.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 
+@Import(ControllerConfig.class)
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
